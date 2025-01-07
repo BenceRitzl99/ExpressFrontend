95c50d08d9279c87f4538a0b50d5e56efa78617a
Ritzl <benceritzl@gmail.com>
2025-01-07 10:53:54 +0100

    2025-01-07

---
+++ src/app/auth.service.ts
@@ -0,0 +1,90 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private api="https://localhost:3000/users/"
+  private loggedUser:any
+  private userSub= new Subject()
+  private httpOptions = {
+    withCredentials: true
+  }
+  constructor(private http:HttpClient) { }
+
+
+  getLoggedUser(){
+    return this.userSub
+  }
+
+  signIn(email:string, password:string){
+    let body ={
+      email:email,
+      password:password
+    }
+
+   
+
+    this.http.post(this.api+"signin", body, this.httpOptions).subscribe(
+      {
+        next:(res)=>{
+          console.log(res)
+          this.loggedUser=res
+          this.userSub.next(this.loggedUser)
+        },
+        error:(err)=>{
+          console.log(err)
+          this.loggedUser=null
+          this.userSub.next(this.loggedUser)
+        }
+      }
+    )
+  }
+
+  // logout(){
+  //         this.loggedUser=null
+  //         this.userSub.next(this.loggedUser)
+  // }
+
+  signUp(email:string, password:string){
+    let body ={
+      email:email,
+      password:password
+    }
+    this.http.post(this.api+"signup", body).subscribe(
+      {
+        next:(res)=>console.log(res),
+        error:(err)=>console.log(err)
+      }
+    )
+  }
+
+  getSecret(){
+    if (this.loggedUser){
+      // let headers=new HttpHeaders({"Authorization":this.loggedUser?.accessToken||"probaToken"})
+      this.http.get(this.api+"secretdata", this.httpOptions).subscribe(
+        {
+          next:(res)=>console.log(res),
+          error:(err)=>console.log(err)
+        }
+      )
+  }
+  }
+
+  logout(){
+    this.http.post(this.api+"logout",{}, this.httpOptions).subscribe(
+      {
+      next:(res)=>{
+        console.log(res)
+        this.loggedUser=null
+        this.userSub.next(this.loggedUser)
+      },
+      error:(err)=>console.log(err)
+      }
+    )
+  }
+
+
+}
